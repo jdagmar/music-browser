c6deb22fa38d892535dd2fa197f213047fd9fbcf
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-03-22 14:53:45 +0100

    first draft of getTracks function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const Api = {
     getArtists: () => Api.get('artists'),
     getAlbums: () => Api.get('albums'),
+    getTracks: () => Api.get('tracks'),
     get(type){
         return fetch(`https://folksa.ga/api/${type}?key=flat_eric`)
             .then(response => response.json())
@@ -33,8 +34,20 @@ const View = {
 
         const albumList = document.getElementById('all-albums');
         listAllAlbums.forEach(albumItem => albumList.appendChild(albumItem));
+    },
+    displayTracks(tracks){
+        const listAllTracks = tracks.map(track => {
+            const trackItem = document.createElement('p');
+            const trackName = document.createTextNode(track.title);
+            trackItem.appendChild(trackName);
+            return trackItem;
+        });
+
+        const trackList = document.getElementById('all-tracks');
+        listAllTracks.forEach(trackItem => trackList.appendChild(trackItem));
     }
 }
 
 Api.getArtists().then(artists => View.displayArtists(artists));
 Api.getAlbums().then(albums => View.displayAlbums(albums));
+Api.getTracks().then(tracks => View.displayTracks(tracks));
