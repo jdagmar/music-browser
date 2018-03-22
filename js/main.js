1febebe710e38713b35a89c7c77ef2849428a6bd
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-03-22 14:37:26 +0100

    first draft of get artists function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
+const Api = {
+    getArtists: () => Api.get('artists'),
+    get(type){
+        return fetch(`https://folksa.ga/api/${type}?key=flat_eric`)
+            .then(response => response.json())
+            .catch(error => {
+                console.log('error');
+                return [];
+            });
+    }
+}
+
 const View = {
+    displayArtists(artists){
+        const listAllArtist = artists.map(artist => {
+            const artistItem = document.createElement('p');
+            const artistName = document.createTextNode(artist.name);
+            artistItem.appendChild(artistName);
+            return artistItem;
+        });
 
+        const artistsList = document.getElementById('all-artists');
+        listAllArtist.forEach(artistItem => artistsList.appendChild(artistItem));
+    }
+    
 }
 
-const Model = {
-
-}
+Api.getArtists().then(artists => View.displayArtists(artists));
